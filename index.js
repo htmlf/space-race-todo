e087159f1da06f60545b09998e5f3fa7cfc33e6e
Subhajit Sahu <wolfram77@gmail.com>
2015-10-17 12:42:43 +0530

    index.js created (server)

---
+++ index.js
@@ -0,0 +1,21 @@
+/* @wolfram77 */
+/* INDEX - main server code */
+
+// required modules
+var express = require('express');
+
+// init express
+var app = express();
+
+
+// url interface
+app.all('/', function(req, res) {
+	res.sendFile(__dirname+'/assets/index.html');
+});
+app.use(express.static(__dirname+'/assets'));
+
+
+// ready
+var server = app.listen(80, function() {
+	console.log('miver>> ready!');
+});
