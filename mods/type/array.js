ba3aeedf9bb9393f57a91419c7446a572a6f3f2d
Subhajit Sahu <wolfram77@gmail.com>
2015-10-18 00:39:28 +0530

    closure added around type/array.js

--- mods/type/array.js
+++ mods/type/array.js
@@ -1,34 +1,36 @@
 /* @wolfram77 */
 /* ARRAY - provides basic array functions */
 
+(function() {
 
-// fill values
-if(!Array.prototype.fill) Array.prototype.fill = function(val, bgn, end) {
-	for(var i=bgn||0, I=end||this.length; i<I; i++)
-		this[i] = val;
-};
+	// fill values
+	if(!Array.prototype.fill) Array.prototype.fill = function(val, bgn, end) {
+		for(var i=bgn||0, I=end||this.length; i<I; i++)
+			this[i] = val;
+	};
 
-// create from iterable
-if(!Array.from) Array.from = function(lst, fn, ths) {
-	var k, a = [];
-	fn = fn || (function(v) { return v; });
-	ths = ths || this;
-	for(k in lst)
-		a.push(fn.call(ths, k, lst[k]));
-	return a;
-};
+	// create from iterable
+	if(!Array.from) Array.from = function(lst, fn, ths) {
+		var k, a = [];
+		fn = fn || (function(v) { return v; });
+		ths = ths || this;
+		for(k in lst)
+			a.push(fn.call(ths, k, lst[k]));
+		return a;
+	};
 
-// append an array
-Array.prototype.append = function(arr) {
-	this.push.apply(this, arr);
-};
+	// append an array
+	Array.prototype.append = function(arr) {
+		this.push.apply(this, arr);
+	};
 
-// last item
-Array.prototype.last = function() {
-	return this[this.length-1];
-};
+	// last item
+	Array.prototype.last = function() {
+		return this[this.length-1];
+	};
 
 
-// ready!
-if(module) module.exports = Array;
-console.log('array> ready!');
+	// ready!
+	if(module) module.exports = Array;
+	console.log('array> ready!');
+})();
