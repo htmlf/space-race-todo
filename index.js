9bc0d8f65eb5a2777bf0b191dbac7558eac7af75
Subhajit Sahu <wolfram77@gmail.com>
2015-10-18 22:12:46 +0530

    math/line.js server test removed

--- index.js
+++ index.js
@@ -24,16 +24,4 @@ app.use(express.static(__dirname+'/mods'));
 // ready
 var server = app.listen(80, function() {
 	console.log('miver>> ready!');
-	var l1 = $line.define([0,0], [2,2]);
-	var l2 = $line.define([0,2], [2,0]);
-	var l1d = $line.distance(l1, [1,0]);
-	var l2d = $line.distance(l2, [1,2]);
-	var p12 = $line.intersect(l1, l2);
-	var l3 = $line.parallel(l1, [2,1]);
-	var l4 = $line.perpendicular(l3, [2,1]);
-	var p34 = $line.intersect(l3, l4);
-	var p56 = $line.intersectRanged([[0,0], [2,2]], [[0,5], [5,0]]);
-	console.log('l1=%j, l2=%j', l1, l2);
-	console.log('l1d=%d, l2d=%d, p12=%j', l1d, l2d, p12);
-	console.log('l3=%j, l4=%j, p34=%j, p56=%j', l3, l4, p34, p56);
 });
