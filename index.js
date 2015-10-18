a801aab7ffcdd145435eccef51864b34ae506ac6
Subhajit Sahu <wolfram77@gmail.com>
2015-10-18 18:38:20 +0530

    type/table.js test removed

--- index.js
+++ index.js
@@ -23,12 +23,4 @@ app.use(express.static(__dirname+'/mods'));
 // ready
 var server = app.listen(80, function() {
 	console.log('miver>> ready!');
-	var cwd = {'name': ['a', 'b'], 'age': [1, 2]};
-	console.log('cwd=%j', cwd);
-	console.log('cols=%j, rows=%d', $table.cols(cwd), $table.rows(cwd));
-	var rwd = $table.rowwise(cwd);
-	console.log('rwd=%j', rwd);
-	console.log('cols=%j, rows=%d', $table.cols(rwd), $table.rows(rwd));
-	cwd = $table.colwise(rwd);
-	console.log('cwd=%j', cwd);
 });
