03fbdffcdd3c18cd7490ef9e2007a3d4de6646bf
Subhajit Sahu <wolfram77@gmail.com>
2015-10-18 22:23:09 +0530

    math/vector.js added

---
+++ mods/math/vector.js
@@ -0,0 +1,61 @@
+/* @wolfram77 */
+/* VECTOR - vector related operations */
+
+var $math = $math || {};
+$math.vector = {
+
+	// from point p to q
+	'define': function(p, q) {
+		return this.sub(q, p);
+	},
+
+	// to polar form
+	'toPolar': function(a) {
+		return [this.length(a), this.angle(a)];
+	},
+
+	// to cartesian form
+	'toCartesian': function(a) {
+		return [a[0]*Math.cos(a[1]), a[1]*Math.cos(a[0])];
+	},
+
+	// length
+	'length': function(a) {
+		return Math.sqrt(a[0]*a[0]+a[1]*a[1]);
+	},
+
+	// angle
+	'angle': function(a) {
+		return Math.atan2(a[1], a[0]);
+	},
+
+	// addition
+	'add': function(a, b) {
+		return [a[0]+b[0], a[1]+b[1]];
+	},
+
+	// subtraction
+	'sub': function(a, b) {
+		return [a[0]-b[0], a[1]-b[1]];
+	},
+
+	// dot product
+	'dot': function(a, b) {
+		return a[0]*b[0]+a[1]*b[1];
+	},
+
+	// cross product
+	'cross': function(a, b) {
+		return a[0]*b[1]-a[1]*b[0];
+	},
+
+	// rotate
+	'rotate': function(a, t) {
+		return [a[0]*Math.cos(t)-a[1]*Math.sin(t), a[0]*Math.sin(t)+a[1]*Math.cos(t)]
+	},
+
+	// scale
+	'scale': function(a, f) {
+		return [f*a[0], f*a[1]];
+	}
+};
