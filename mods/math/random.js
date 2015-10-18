19a64425419dd9726e48e4994d8566ccf96ccd3e
Subhajit Sahu <wolfram77@gmail.com>
2015-10-18 22:05:56 +0530

    math/random.js updated design

--- mods/math/random.js
+++ mods/math/random.js
@@ -1,12 +1,18 @@
 /* @wolfram77 */
 /* RANDOM - basic random functions */
 
-var $math = $math || {};
-$math.random = {
+(function(g) {
+
+	var $ = {};
 
 	// symmetric powered
-	'sympow': function(rng, pow) {
+	$.sympow = function(rng, pow) {
 		var v = Math.pow(Math.random(), pow);
 		return rng[0] + (rng[1]-rng[0])*(0.5 + 0.5*(Math.random()>0.5? v:-v));
-	}
-};
+	};
+
+	// ready
+	if(typeof module!=='undefined') module.exports = $;
+	else (g.$type=g.$type||{}).random = $;
+	console.log('random> ready!');
+})(this);
