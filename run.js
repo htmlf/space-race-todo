f25f4df813107bb2dc75fd62bc8acebd44846de9
Subhajit Sahu <wolfram77@gmail.com>
2015-10-19 11:50:59 +0530

    run.js installdeps done

--- run.js
+++ run.js
@@ -2,7 +2,7 @@
 /* RUN - build and run the server */
 
 // required modules
-var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 var fs = require('fs');
 
 
@@ -11,3 +11,21 @@ var direxists = function(path) {
 	try { return fs.lstatSync(path).isDirectory(); }
 	catch(e) { return false; }
 };
+
+// install dependencies
+var installdeps = function() {
+	var has = direxists('node_modules');
+	if(has) {
+		var pkg = JSON.parse(fs.readFileSync('package.json'));
+		var deps = (pkg.dependencies||[]).concat(pkg.devDependencies||[]);
+		for(var i=0; i<deps.length; i++)
+			has = has && direxists('node_modules/'+deps[i]);
+	}
+	if(has) return;
+	var child = spawn('npm', ['install']);
+	child.stdout.pipe(process.stdout);
+	child.stderr.pipe(process.stderr);
+};
+
+// ready
+installdeps();
