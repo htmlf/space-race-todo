0f46291a960f96ced25e1a5c89d571412f2ff13d
Subhajit Sahu <wolfram77@gmail.com>
2015-10-19 12:02:32 +0530

    run.js bug fixed and added server run code

--- run.js
+++ run.js
@@ -12,20 +12,27 @@ var direxists = function(path) {
 	catch(e) { return false; }
 };
 
+// run shell command
+var shell = function(cmd, params) {
+	var child = spawn(cmd, params);
+	child.stdout.pipe(process.stdout);
+	child.stderr.pipe(process.stderr);
+};
+
 // install dependencies
 var installdeps = function() {
 	var has = direxists('node_modules');
 	if(has) {
 		var pkg = JSON.parse(fs.readFileSync('package.json'));
-		var deps = (pkg.dependencies||[]).concat(pkg.devDependencies||[]);
+		var deps = (Object.keys(pkg.dependencies||{})).concat(Object.keys(pkg.devDependencies||{}));
 		for(var i=0; i<deps.length; i++)
 			has = has && direxists('node_modules/'+deps[i]);
 	}
 	if(has) return;
-	var child = spawn('npm', ['install']);
-	child.stdout.pipe(process.stdout);
-	child.stderr.pipe(process.stderr);
+	console.log('run> install %j', deps);
+	shell('npm', ['install']);
 };
 
 // ready
 installdeps();
+shell('node', ['index']);
