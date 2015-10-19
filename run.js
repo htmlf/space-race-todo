1248e603abb69f6b23a8bd26b39ee5f2e5fc5ec3
Subhajit Sahu <wolfram77@gmail.com>
2015-10-19 19:52:00 +0530

    run.js done

--- run.js
+++ run.js
@@ -2,9 +2,12 @@
 /* RUN - build and run the server */
 
 // required modules
-var spawn = require('child_process').spawn;
+var childProcess = require('child_process');
 var fs = require('fs');
 
+
+// shell command lists
+var shellcmds = [];
 
 // check if directory exists
 var direxists = function(path) {
@@ -12,11 +15,23 @@ var direxists = function(path) {
 	catch(e) { return false; }
 };
 
-// run shell command
-var shell = function(cmd, params) {
-	var child = spawn(cmd, params);
+var runshell = function() {
+	if(shellcmds.length===0) return;
+	if(shellcmds[0].start) shellcmds[0].start();
+	var child = childProcess.exec(shellcmds[0].cmd, function(err) {
+		if(err) console.log('shell> %j', err);
+		process.nextTick(runshell);
+		if(shellcmds[0].end) shellcmds[0].end();
+		shellcmds.shift();
+	});
 	child.stdout.pipe(process.stdout);
 	child.stderr.pipe(process.stderr);
+};
+
+// run shell command
+var shell = function(cmd, start, end) {
+	shellcmds.push({'cmd': cmd, 'start': start, 'end': end});
+	if(shellcmds.length===1) runshell();
 };
 
 // install dependencies
@@ -29,10 +44,15 @@ var installdeps = function() {
 			has = has && direxists('node_modules/'+deps[i]);
 	}
 	if(has) return;
-	console.log('run> install %j', deps);
-	shell('npm', ['install']);
+	shell('npm install', function() {
+		console.log('\x1b[36m'+'run> install %j'+'\x1b[0m', deps);
+	}, function() {
+		console.log('\n');
+	});
 };
 
 // ready
 installdeps();
-shell('node', ['index']);
+shell('node index', function() {
+	console.log('\x1b[36m'+'run> server'+'\x1b[0m');
+});
