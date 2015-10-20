a59d8591d59ee8755e5d8a61c773d32db322522e
Subhajit Sahu <wolfram77@gmail.com>
2015-10-20 12:35:12 +0530

    run.js updated to using code/serial.js

--- run.js
+++ run.js
@@ -4,55 +4,45 @@
 // required modules
 var childProcess = require('child_process');
 var fs = require('fs');
+var $serial = require('./mods/code/serial');
 
-
-// shell command lists
-var shellcmds = [];
 
 // check if directory exists
-var direxists = function(path) {
+var dirExists = function(path) {
 	try { return fs.lstatSync(path).isDirectory(); }
 	catch(e) { return false; }
 };
 
-var runshell = function() {
-	if(shellcmds.length===0) return;
-	if(shellcmds[0].start) shellcmds[0].start();
-	var child = childProcess.exec(shellcmds[0].cmd, function(err) {
-		if(err) console.log('shell> %j', err);
-		process.nextTick(runshell);
-		if(shellcmds[0].end) shellcmds[0].end();
-		shellcmds.shift();
+
+// run shell command (serial)
+var shell = function(cmd, fn) {
+	var c = childProcess.exec(cmd, fn);
+	c.stdout.pipe(process.stdout);
+	c.stderr.pipe(process.stderr);
+};
+
+// has dependencies?
+var hasDeps = function() {
+	if(!dirExists('node_modules')) return false;
+	var pkg = JSON.parse(fs.readFileSync('package.json'));
+	var deps = (Object.keys(pkg.dependencies||{})).concat(Object.keys(pkg.devDependencies||{}));
+	for(var i=0; i<deps.length; i++)
+		if(!dirExists('node_modules/'+deps[i])) return false;
+	return true;
+};
+
+// run program
+var run = function() {
+	var shellser = new $serial();
+	if(!hasDeps()) shellser.run(function() {
+		console.log('\x1b[36m'+'run> installing dependencies'+'\x1b[0m');
+		shell('npm install', function() { console.log('\n'); shellser.end(); });
 	});
-	child.stdout.pipe(process.stdout);
-	child.stderr.pipe(process.stderr);
-};
-
-// run shell command
-var shell = function(cmd, start, end) {
-	shellcmds.push({'cmd': cmd, 'start': start, 'end': end});
-	if(shellcmds.length===1) runshell();
-};
-
-// install dependencies
-var installdeps = function() {
-	var has = direxists('node_modules');
-	if(has) {
-		var pkg = JSON.parse(fs.readFileSync('package.json'));
-		var deps = (Object.keys(pkg.dependencies||{})).concat(Object.keys(pkg.devDependencies||{}));
-		for(var i=0; i<deps.length; i++)
-			has = has && direxists('node_modules/'+deps[i]);
-	}
-	if(has) return;
-	shell('npm install', function() {
-		console.log('\x1b[36m'+'run> install %j'+'\x1b[0m', deps);
-	}, function() {
-		console.log('\n');
+	shellser.run(function() {
+		console.log('\x1b[36m'+'run> starting server'+'\x1b[0m');
+		shell('node index', function() { console.log('\x1b[36m'+'run> server stopped'+'\x1b[0m'); shellser.end(); });
 	});
 };
 
 // ready
-installdeps();
-shell('node index', function() {
-	console.log('\x1b[36m'+'run> server'+'\x1b[0m');
-});
+run();
