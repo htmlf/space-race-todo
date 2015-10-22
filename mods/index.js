fee79e39bce246f4682e28aa199cfa850d1e25f0
Subhajit Sahu <wolfram77@gmail.com>
2015-10-22 15:57:45 +0530

    remove socket.io test from index.js

--- mods/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	var skt = io();
-	skt.on('msg', function(msg) {
-		$('#msg').append('<li>'+msg+'</li>');
-	});
-	$('#btn').click(function() {
-		skt.emit('msg', $('#txt').val());
-	});
-});
