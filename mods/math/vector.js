48c3049c0c56587f73af80a7f3d1d897983832d0
Subhajit Sahu <wolfram77@gmail.com>
2015-10-24 15:51:22 +0530

    math/vector.js added unit vector function

--- mods/math/vector.js
+++ mods/math/vector.js
@@ -30,6 +30,12 @@
 		return Math.atan2(a[1], a[0]);
 	};
 
+	// unit (vector)
+	$.unit = function(a) {
+		var l = this.length(a);
+		return [a[0]/l, a[1]/l];
+	};
+
 	// addition
 	$.add = function(a, b) {
 		return [a[0]+b[0], a[1]+b[1]];
