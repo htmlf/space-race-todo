77e1408efe9d042f0532db4f1e940e70d3f7c72d
Subhajit Sahu <wolfram77@gmail.com>
2015-10-26 18:15:59 +0530

    graphics/scene.js created

---
+++ mods/graphics/scene.js
@@ -0,0 +1,39 @@
+/* @wolfram77 */
+/* SCENE - defines a multi-layered scene */
+
+(function(g) {
+
+	var $ = function(v) {
+		v = v||{};
+		// layers
+		this.ls = new Array(v.nlyr||3);
+		for(var i=0, I=this.ls.length; i<I; i++)
+			this.ls[i] = [];
+	};
+	var p = $.prototype;
+
+	// add item to scene
+	p.add = function(l, itm) {
+		this.ls[l].push(itm);
+	};
+
+	// remove
+	p.remove = function(l, itm) {
+		var lv = this.ls[l];
+		for(var i=0, I<lv.length; i<I; i++)
+			if(lv[i]===itm) { lv.splice(i, 1); return; }
+	};
+
+	// get (in range)
+	p.get = function(l, r) {
+		var $r = $math.rect, lv=this.ls[l], a=[];
+		for(var i=0, I=lv.length; i<I; i++)
+			if($r.overlaps(r, lv[i]._rng)) a.push(lv[i]);
+		return a;
+	};
+
+	// ready
+	if(typeof module!=='undefined') module.exports = $;
+	else (g.$graphics=g.$graphics||{}).scene = $;
+	console.log('graphics.scene> ready!');
+})(this);
